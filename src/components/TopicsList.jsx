fbb9ebf9829f1daf35a5b82f1e8565657dcc19eb
kingsofted <hongjun17110@gmail.com>
2023-09-18 17:45:35 +0800

    d

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -27,11 +27,11 @@ const getTopics = async() =>{
 
 const TopicsList = async() => {
 
-    const topics = await getTopics();
+    // const topics = await getTopics();
 
   return (
     <>
-    {topics.map((t)=>(
+    {/* {topics.map((t)=>(
         <div className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start' key={t._id}>
             <div>
                 <h2 className='font-bold text-2xl'>{t.title}</h2>
@@ -46,7 +46,15 @@ const TopicsList = async() => {
                 </Link>
             </div>
         </div>
-    ))}
+    ))} */}
+        <div className='p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start'>
+            <div>
+                <h2 className='font-bold text-2xl'>Hello</h2>
+                <div>Hello</div>
+                <Image src="https://media.istockphoto.com/id/1173279965/photo/smart-city-and-abstract-dot-point-connect-with-gradient-line.jpg?s=612x612&w=0&k=20&c=WkMJQe9YWZ7K575cA9OdqB4q6XrqlrRMafE2UbF9MT4=" width={300} height={700} alt='image' priority/>
+            </div>
+
+        </div>
     </>
   )
 }
